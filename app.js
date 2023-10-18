af937cebf44f003bb474f15df7550622c7de924a
Spencer Kwon <spencerkwon92@gmail.com>
2023-10-19 00:42:04 +0900

    Changed package.json

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const passport = require("passport");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const path = require("path");
+const hpp = require("hpp");
+const helmet = require("helmet");
 
 const postsRouter = require("./routes/posts");
 const postRouter = require("./routes/post");
@@ -25,11 +27,18 @@ db.sequelize
   .catch(console.error);
 passportConfig();
 
+if(process.env.NODE_ENV === "production"){
+  app.use(morgan("combined"));
+  app.use(hpp());
+  app.use(helmet());
+}else{
+  app.use(morgan("dev"));
+}
+
 //middlewares...
-app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", 'mymemories.com'],
     credentials: true,
   })
 );
